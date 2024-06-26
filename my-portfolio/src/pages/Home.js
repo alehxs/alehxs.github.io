3909d206e137b1b9a369d5d101eb475307c15ea3
Alex <86182397+alehxs@users.noreply.github.com>
2024-06-26 14:36:43 -0500

    Add website

---
+++ my-portfolio/src/pages/Home.js
@@ -0,0 +1,14 @@
+// src/pages/Home.js
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Alexander Sangurima</h1>
+      <p>Hi, I'm a Computer Science student. Check out my projects and learn more about me!</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ my-portfolio/src/pages/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <ul>
+        <li>Project 1: Description</li>
+        <li>Project 2: Description</li>
+        <li>Project 3: Description</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Projects;
